e8ca48e4792f8e35f49e7dd8dada2a875d4dc32d
pradeep.sounderrajan <pradeep.sounderrajan@lloydsbanking.com>
2020-01-10 21:19:05 +0000

    feat: webpack commom and index file

---
+++ webpack/webpack.common.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: './src/client/index.js',
+  },
+  output: {
+    path: path.resolve(__dirname, '../_dist'),
+    filename: 'bundle.js',
+    publicPath: '.',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "My First Program",
+      template: path.resolve(__dirname, '../templates/template.html'),
+    }),
+  ],
+};
