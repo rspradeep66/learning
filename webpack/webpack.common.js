a2d3fa3f6ae986a2b99b9f22f0013e5da950afb6
pradeep.sounderrajan <pradeep.sounderrajan@lloydsbanking.com>
2020-01-20 21:00:13 +0000

    feat: webpack plugins added

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,5 +1,8 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const Visualizer = require('webpack-visualizer-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: {
@@ -21,9 +24,18 @@ module.exports = {
     ],
   },
   plugins: [
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       title: "My First Program",
       template: path.resolve(__dirname, '../templates/template.html'),
+    }),
+    new BundleAnalyzerPlugin({
+      openAnalyzer: false,
+      analyzerMode: 'static',
+      reportFilename: path.resolve(__dirname, '../_dist/bundle.html'),
+    }),
+    new Visualizer({
+      filename: './visualizer.html',
     }),
   ],
 };
