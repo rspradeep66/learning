525a39bd4d4b4a83492c67185f6487bbd495ec7d
pradeep.sounderrajan <pradeep.sounderrajan@lloydsbanking.com>
2020-01-22 20:49:26 +0000

    feat: counter display created

--- src/client/index.js
+++ src/client/index.js
@@ -1,10 +1,35 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 
-class App extends Component {
+class CounterApp extends React.Component {
+  state = {
+    count: 0,
+  };
+
+  increment = () => {
+    console.log("increment");
+    const currentVal = this.state.count;
+    this.setState({
+      count: currentVal + 1,
+    });
+  }
+
+  decrement = () => {
+    console.log("Decrement");
+    const currentVal = this.state.count;
+    this.setState({
+      count: currentVal - 1,
+    });
+
+  }
+
   render() {
-    return <h1>Hello World !!!</h1>;
+    return <div>
+      <p>{this.state.count}</p>
+      <button onClick={this.increment}>+</button>
+      <button onClick={this.decrement}>-</button>
+    </div>
   }
 }
 
-render(<App />, document.getElementById('root'));
+render(<CounterApp />, document.getElementById('root'));
